3bed1ab69b023aa4c2763e42257d713b809a6272
MarinaZelenska <osychenkomarina@gmail.com>
2024-08-29 16:41:15 +0200

    homework 3  function pow

---
+++ homework_3.js
@@ -0,0 +1,14 @@
+function pow(numberForDegree, degree) {
+    let result = 1;
+    const degreeAbsolute = Math.abs(degree)
+    for (let i = 0; i < degreeAbsolute; i++) {
+        result *= numberForDegree;
+    }
+
+    return degree < 0 ? 1 / result : result;
+}
+const zero = pow(2, 0) // test for degree 0 -> return 1
+const one = pow(2, 1) // test for degree 1 -> return input number
+const cube = pow(3, 3) 
+const negative = pow(-2, 3) // test for negative number -> return -8
+const negativeDegree = pow(2, -3) // test for negative degree -> return 0.125
