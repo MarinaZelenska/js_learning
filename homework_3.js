745275bbfcb5f44b134385020fd75d7dd5c083df
MarinaZelenska <osychenkomarina@gmail.com>
2024-08-29 16:46:14 +0200

    added new line

--- homework_3.js
+++ homework_3.js
@@ -7,6 +7,8 @@ function pow(numberForDegree, degree) {
 
     return degree < 0 ? 1 / result : result;
 }
+
+
 const zero = pow(2, 0) // test for degree 0 -> return 1
 const one = pow(2, 1) // test for degree 1 -> return input number
 const cube = pow(3, 3) 
