fe99f8790fc9c42ddafadc3aacff94294072d93e
MarinaZelenska <osychenkomarina@gmail.com>
2024-09-05 11:49:53 +0200

    add sevice object

---
+++ homework_5_2.js
@@ -0,0 +1,42 @@
+var services = {
+    "стрижка": "60 грн",
+    "гоління": "80 грн",
+    "Миття голови": "100 грн",
+
+    price(){
+        totalPrice = 0;
+        for (let key in this){
+            let value = parseInt(this[key]);
+            if (!isNaN(value)) { 
+                totalPrice += value;
+            }
+        }
+        return `${totalPrice} грн`;
+        },
+
+    minPrice(){
+        let minPrice = Infinity;
+        for (let key in this) {
+            let value = parseInt(this[key]);
+            if (!isNaN(value) && value < minPrice) {
+                minPrice = value;
+            }
+        }
+        return `${minPrice} грн`;
+    },
+
+    maxPrice() {
+        let maxPrice = 0;
+        for (let key in this) {
+            let value = parseInt(this[key]);
+            if (!isNaN(value) && value > maxPrice) {
+                maxPrice = value;
+            }
+        }
+        return `${maxPrice} грн`;
+    }
+}
+
+console.log(services.price())
+console.log(services.maxPrice())
+console.log(services.minPrice())
